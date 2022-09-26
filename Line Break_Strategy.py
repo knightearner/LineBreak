f4aff167d26885461f4637844122eb8c482d3fbc
knightearner <114371387+knightearner@users.noreply.github.com>
2022-09-26 07:01:14 +0530

    Add files via upload

---
+++ Line Break_Strategy.py
@@ -0,0 +1,72 @@
+from py5paisa import *
+from datetime import *
+from stocktrends import indicators
+import pandas as pd
+import time
+
+def Line_Break():
+
+    cred = {
+        "APP_NAME": "5P51419361",
+        "APP_SOURCE": "11179",
+        "USER_ID": "FKTvPb6GrxX",
+        "PASSWORD": "Rf7SoLvxXcj",
+        "USER_KEY": "LCHhAHfRrkeDwQkjHyGtQGFmBkl1h50T",
+        "ENCRYPTION_KEY": "c5yN3Ny1k4zj272fI40YDzHrF4Q1dics"
+    }
+
+    client = FivePaisaClient(email="mondaldebojit21@gmail.com", passwd="Rintu!1995", dob="19951021", cred=cred)
+    client.login()
+
+
+
+    today = date.today()
+
+    end = str(today)
+    start = str(today - timedelta(days=5))
+
+    df = client.historical_data('N', 'C', 999920000, '5m', start, end)
+
+    df.rename(columns={
+        'Open': 'open',
+        'High': 'high',
+        'Low': 'low',
+        'Close': 'close',
+        'Datetime': 'date'
+    }, inplace=True)
+
+    lb = indicators.LineBreak(df)
+    lb.line_number = 3
+    data = lb.get_ohlc_data()
+    df = pd.merge(df, data[['date', 'uptrend']], on='date', how='outer')
+    for i in range(len(df)):
+        if df['uptrend'][i] not in [True, False]:
+            df['uptrend'][i] = df['uptrend'][i - 1]
+
+    Nifty_Future_Code=51951
+    Nifty_Future_Lot=50
+
+    try:
+        print(str(today))
+
+        if (df['uptrend'][len(df) - 1] != df['uptrend'][len(df) - 2]) and df['uptrend'][len(df) - 1] == True:
+            client.place_order(OrderType='B', Exchange='N', ExchangeType='D', ScripCode=Nifty_Future_Code, Qty=Nifty_Future_Lot, Price=0)
+            print(str(today), ' BUY @ ')
+
+        elif (df['uptrend'][len(df) - 1] != df['uptrend'][len(df) - 2]) and df['uptrend'][len(df) - 1] == False:
+            client.place_order(OrderType='S', Exchange='N', ExchangeType='D', ScripCode=Nifty_Future_Code, Qty=Nifty_Future_Lot, Price=0)
+            print(str(today), 'SELL @ ')
+
+        else:
+            print('NO order Placed')
+
+    except Exception as e:
+        print(str(e))
+
+
+
+
+if __name__=='__main__':
+    while True:
+        Line_Break()
+        time.sleep(10)
