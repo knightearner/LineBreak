984db0b02171aaf1a96a0436ea63b4a1e5ef7cb7
knightearner <114371387+knightearner@users.noreply.github.com>
2022-09-30 10:04:53 +0530

    Update Line_Break_Strategy.py

--- Line_Break_Strategy.py
+++ Line_Break_Strategy.py
@@ -3,17 +3,17 @@ from datetime import *
 import pytz
 import easyimap as e
 
-buy_email_='buy_knightearner@outlook.com'
-#sell_email_='mondaldebojit21@outlook.com'
+#buy_email_='buy_knightearner@outlook.com'
+sell_email_='mondaldebojit21@outlook.com'
 
-buy_password='Debojit@1995'
-#sell_password='Rintu!1995'
+#buy_password='Debojit@1995'
+sell_password='Rintu!1995'
 
-server_buy=e.connect('outlook.office365.com',buy_email_,buy_password)
-#server_sell=e.connect('outlook.office365.com',sell_email_,sell_password)
+#server_buy=e.connect('outlook.office365.com',buy_email_,buy_password)
+server_sell=e.connect('outlook.office365.com',sell_email_,sell_password)
 
-email_buy=server_buy.mail(server_buy.listids()[0])
-#email_sell=server_sell.mail(server_sell.listids()[0])
+#email_buy=server_buy.mail(server_buy.listids()[0])
+email_sell=server_sell.mail(server_sell.listids()[0])
 
-print(email_buy.date)
-#print(email_sell.date)
+#print(email_buy.date)
+print(email_sell.date)
