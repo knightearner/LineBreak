d4c27cc67f1cf032eb7d2ee1bd5e177e824ff787
knightearner <114371387+knightearner@users.noreply.github.com>
2022-10-13 00:42:49 +0530

    Update Line_Break_Strategy.py

--- Line_Break_Strategy.py
+++ Line_Break_Strategy.py
@@ -5,58 +5,36 @@ import easyimap as e
 import time
 
 
-def buy_login():
-    buy_email_='buy_knightearner@outlook.com'
 
-    buy_password='Debojit@1995'
+def login():
+    _email_='mondaldebojit21@outlook.com'
 
-    server_buy=e.connect('outlook.office365.com',buy_email_,buy_password)
+    _password='Rintu!1995'
 
-    return server_buy
+    server_=e.connect('outlook.office365.com',_email_,_password)
 
+    return server_
 
-def sell_login():
-    sell_email_='mondaldebojit21@outlook.com'
+def Line_Break(server_):
 
-    sell_password='Rintu!1995'
+   
+    email_=server_.mail(server_.listids()[0])
 
-    server_sell=e.connect('outlook.office365.com',sell_email_,sell_password)
-
-    return server_sell
-
-
-
-def Line_Break(server_buy,server_sell):
-
-    email_buy=server_buy.mail(server_buy.listids()[0])
-    email_sell=server_sell.mail(server_sell.listids()[0])
-
-
-    format = '%d %b %Y %H:%M'  # The format
-    time_buy= datetime.strptime(email_buy.date[5:-9], format)
-    time_sell= datetime.strptime(email_sell.date[5:-9], format)
-
-    time_now=datetime.now(pytz.timezone('UTC'))
-    format='%Y-%m-%d %H:%M'
-    time_now = datetime.strptime(str(time_now)[:16], format)
-    time_now_list=[time_now+timedelta(minutes=i) for i in [-2,-1,0,1,2]]
-    
+    time_now=datetime.now(pytz.timezone('Asia/Kolkata'))
     print('TIME NOW : ',time_now)
     
     flag=''
-    if time_sell in time_now_list and email_sell.title[7:]=='SELL':
+    if int(email_.title[-2:])==-1:
         flag='SELL'
-        print('TIME SELL : ',time_sell)
-    elif time_buy in time_now_list and email_buy.title[7:]=='BUY':
+    elif int(email_.title[-2:])==1:
         flag='BUY'
-        print('TIME BUY : ',time_buy)
     else:
         flag=''
 
     Nifty_Future_Code=52309
     Nifty_Future_Lot=300
+    
     print('Flag Status: ',flag)
-    print(str(datetime.now(pytz.timezone('Asia/Kolkata'))))
     
     if flag in ['BUY','SELL']:
         cred = {
@@ -89,12 +67,12 @@ def Line_Break(server_buy,server_sell):
 
 
 if __name__=='__main__':
-    server_buy_=buy_login()
-    server_sell_=sell_login()
+    server_=login()
+    
     while True:
         if datetime.now(pytz.timezone('Asia/Kolkata')).hour==9 and datetime.now(pytz.timezone('Asia/Kolkata')).minute>=15:
-                Line_Break(server_buy_,server_sell_)
+                Line_Break(server_)
                 time.sleep(10)
         elif datetime.now(pytz.timezone('Asia/Kolkata')).hour>9 and datetime.now(pytz.timezone('Asia/Kolkata')).hour<16:
-                Line_Break(server_buy_,server_sell_)
+                Line_Break(server_)
                 time.sleep(10)
