d3b57b0d79c9934893b516317f637ffde7dab660
knightearner <114371387+knightearner@users.noreply.github.com>
2022-11-07 11:52:04 +0530

    Update Line_Break_Strategy.py

--- Line_Break_Strategy.py
+++ Line_Break_Strategy.py
@@ -3,76 +3,121 @@ from datetime import *
 import pytz
 import easyimap as e
 import time
-
+import pandas as pd
 
 
 def email_login():
-    _email_='mondaldebojit21@outlook.com'
+    _email_ = 'mondaldebojit21@outlook.com'
 
-    _password='Rintu!1995'
+    _password = 'Rintu!1995'
 
-    server_=e.connect('outlook.office365.com',_email_,_password)
+    server_ = e.connect('outlook.office365.com', _email_, _password)
 
     return server_
 
+def closest_index(lst, K):
+    return min(range(len(lst)), key=lambda i: abs(lst[i] - K))
+
+
 def Line_Break(server_):
+    email_ = server_.mail(server_.listids()[0])
 
-   
-    email_=server_.mail(server_.listids()[0])
+    time_now = datetime.now(pytz.timezone('Asia/Kolkata'))
+    print('TIME NOW : ', time_now)
 
-    time_now=datetime.now(pytz.timezone('Asia/Kolkata'))
-    print('TIME NOW : ',time_now)
-    
-    flag=''
+    option=''
+    flag = ''
     if 'SELL' in email_.body:
-        flag='SELL'
+        flag = 'SELL'
+        option='CE'
     elif 'BUY' in email_.body:
-        flag='BUY'
+        flag = 'BUY'
+        option='PE'
     else:
-        flag=''
+        flag = ''
+    print(flag)
 
-    Nifty_Future_Code=51951
-    Nifty_Future_Lot=50
-    
-    print('Flag Status: ',flag)
-    
-    if flag in ['BUY','SELL']:
-        cred = {
-        "APP_NAME": "5P51419361",
-        "APP_SOURCE": "11179",
-        "USER_ID": "FKTvPb6GrxX",
-        "PASSWORD": "Rf7SoLvxXcj",
-        "USER_KEY": "LCHhAHfRrkeDwQkjHyGtQGFmBkl1h50T",
-        "ENCRYPTION_KEY": "c5yN3Ny1k4zj272fI40YDzHrF4Q1dics"
-    }
+    df = pd.read_csv("scripmaster-csv-format.csv")
+    date_='10 Nov 2022'
+    print('Flag Status: ', flag)
 
-        client = FivePaisaClient(email="mondaldebojit21@gmail.com", passwd="Rintu!1995", dob="19951021", cred=cred)
-        client.login()
-    
-    if flag=='BUY':
+    cred = {
+            "APP_NAME": "5P51419361",
+            "APP_SOURCE": "11179",
+            "USER_ID": "FKTvPb6GrxX",
+            "PASSWORD": "Rf7SoLvxXcj",
+            "USER_KEY": "LCHhAHfRrkeDwQkjHyGtQGFmBkl1h50T",
+            "ENCRYPTION_KEY": "c5yN3Ny1k4zj272fI40YDzHrF4Q1dics"
+        }
 
-        if client.positions()[0]['NetQty']<0:
-            client.place_order(OrderType='B', Exchange='N', ExchangeType='D', ScripCode=Nifty_Future_Code, Qty=Nifty_Future_Lot, Price=0)
-            time.sleep(2)
-            client.place_order(OrderType='B', Exchange='N', ExchangeType='D', ScripCode=Nifty_Future_Code, Qty=Nifty_Future_Lot, Price=0)
-            print(' BUY Order Placed')
+    client = FivePaisaClient(email="mondaldebojit21@gmail.com", passwd="Rintu!1995", dob="19951021", cred=cred)
+    client.login()
 
-    elif flag=='SELL':
+    client.positions()
+    a = [{"Exchange": "N", "ExchangeType": "C", "Symbol": "NIFTY"}]
+    nifty_ltp = (client.fetch_market_depth_by_symbol(a)['Data'][0]['LastTradedPrice'])
 
-        if client.positions()[0]['NetQty']>0:
-            client.place_order(OrderType='S', Exchange='N', ExchangeType='D', ScripCode=Nifty_Future_Code, Qty=Nifty_Future_Lot, Price=0)
-            time.sleep(2)
-            client.place_order(OrderType='S', Exchange='N', ExchangeType='D', ScripCode=Nifty_Future_Code, Qty=Nifty_Future_Lot, Price=0)
-            print(' SELL Order Placed')
+    for i in client.positions():
+        if i['NetQty'] < 0:
+            Strikerate = df.loc[df['Scripcode'] == i['Scripcode']]['Strikerate']
+            option_type = df.loc[df['Scripcode'] == i['Scripcode'], 'CpType']
+            if nifty_ltp > (Strikerate + 50):
+                if option_type == 'PE':
+                    client.squareoff_all()
+                    l = list(
+                        df[(df['ISIN'] == 'NIFTY') & (df['CpType'] == 'PE') & (df['Underlyer'] == date_)]['Strikerate'])
+                    Strikerate = l[closest_index(l, nifty_ltp)]
+                    Scripcode = (df[
+                        (df['ISIN'] == 'NIFTY') & (df['CpType'] == 'PE') & (df['Strikerate'] == Strikerate) & (
+                                    df['Underlyer'] == date_)]['Scripcode'])
+                    client.place_order(OrderType='S', Exchange='N', ExchangeType='D', ScripCode=Scripcode, Qty=50,
+                                       Price=0)
+                    print('PE Order Placed')
+                elif option_type == 'CE':
+                    client.squareoff_all()
+                    l = list(
+                        df[(df['ISIN'] == 'NIFTY') & (df['CpType'] == 'CE') & (df['Underlyer'] == date_)]['Strikerate'])
+                    Strikerate = l[closest_index(l, nifty_ltp)]
+                    Scripcode = (df[
+                        (df['ISIN'] == 'NIFTY') & (df['CpType'] == 'CE') & (df['Strikerate'] == Strikerate) & (
+                                    df['Underlyer'] == date_)]['Scripcode'])
+                    client.place_order(OrderType='S', Exchange='N', ExchangeType='D', ScripCode=Scripcode, Qty=50,
+                                       Price=0)
+                    print('CE Order Placed')
+            elif flag == 'BUY':
+                client.squareoff_all()
+                l = list(
+                    df[(df['ISIN'] == 'NIFTY') & (df['CpType'] == 'PE') & (df['Underlyer'] == date_)]['Strikerate'])
+                Strikerate = l[closest_index(l, nifty_ltp)]
+                Scripcode = (df[
+                    (df['ISIN'] == 'NIFTY') & (df['CpType'] == 'PE') & (df['Strikerate'] == Strikerate) & (
+                            df['Underlyer'] == date_)]['Scripcode'])
+                client.place_order(OrderType='S', Exchange='N', ExchangeType='D', ScripCode=Scripcode, Qty=50,
+                                   Price=0)
+                print('PE Order Placed')
+            elif flag == 'SELL':
+                client.squareoff_all()
+                l = list(
+                    df[(df['ISIN'] == 'NIFTY') & (df['CpType'] == 'CE') & (df['Underlyer'] == date_)]['Strikerate'])
+                Strikerate = l[closest_index(l, nifty_ltp)]
+                Scripcode = (df[
+                    (df['ISIN'] == 'NIFTY') & (df['CpType'] == 'CE') & (df['Strikerate'] == Strikerate) & (
+                            df['Underlyer'] == date_)]['Scripcode'])
+                client.place_order(OrderType='S', Exchange='N', ExchangeType='D', ScripCode=Scripcode, Qty=50,
+                                   Price=0)
+                print('CE Order Placed')
 
 
-if __name__=='__main__':
-    server_=email_login()
-    
+
+if __name__ == '__main__':
+    server_ = email_login()
+
     while True:
-        if datetime.now(pytz.timezone('Asia/Kolkata')).hour==9 and datetime.now(pytz.timezone('Asia/Kolkata')).minute>=15:
-                Line_Break(server_)
-                time.sleep(10)
-        elif datetime.now(pytz.timezone('Asia/Kolkata')).hour>9 and datetime.now(pytz.timezone('Asia/Kolkata')).hour<16:
-                Line_Break(server_)
-                time.sleep(10)
+        if datetime.now(pytz.timezone('Asia/Kolkata')).hour == 9 and datetime.now(
+                pytz.timezone('Asia/Kolkata')).minute >= 15:
+            Line_Break(server_)
+            time.sleep(10)
+        elif datetime.now(pytz.timezone('Asia/Kolkata')).hour > 9 and datetime.now(
+                pytz.timezone('Asia/Kolkata')).hour < 16:
+            Line_Break(server_)
+            time.sleep(10)
